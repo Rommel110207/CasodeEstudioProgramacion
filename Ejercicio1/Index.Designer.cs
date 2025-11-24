17b7cf3e854718734072a8853ac0298247bdf703
Rommel Alexander Muñoz Guerrero <rommelmg@uamv.edu.ni>
2025-11-24 08:14:17 -0600

    Los 6 Ejercicios

---
+++ Ejercicio1/Index.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Ejercicio1
+{
+    partial class Index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjercicio1 = new System.Windows.Forms.Button();
+            this.btnEjercicio2 = new System.Windows.Forms.Button();
+            this.btnEjercicio3 = new System.Windows.Forms.Button();
+            this.btnEjercicio4 = new System.Windows.Forms.Button();
+            this.btnEjercicio5 = new System.Windows.Forms.Button();
+            this.btnEjercicio6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEjercicio1
+            // 
+            this.btnEjercicio1.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnEjercicio1.Location = new System.Drawing.Point(30, 39);
+            this.btnEjercicio1.Name = "btnEjercicio1";
+            this.btnEjercicio1.Size = new System.Drawing.Size(211, 64);
+            this.btnEjercicio1.TabIndex = 0;
+            this.btnEjercicio1.Text = "Ejercicio 1 -  Búsqueda Lineal";
+            this.btnEjercicio1.UseVisualStyleBackColor = false;
+            this.btnEjercicio1.Click += new System.EventHandler(this.btnEjercicio1_Click);
+            // 
+            // btnEjercicio2
+            // 
+            this.btnEjercicio2.BackColor = System.Drawing.Color.Magenta;
+            this.btnEjercicio2.Location = new System.Drawing.Point(281, 39);
+            this.btnEjercicio2.Name = "btnEjercicio2";
+            this.btnEjercicio2.Size = new System.Drawing.Size(211, 64);
+            this.btnEjercicio2.TabIndex = 1;
+            this.btnEjercicio2.Text = "Ejercicio 2 -  Búsqueda Binaria";
+            this.btnEjercicio2.UseVisualStyleBackColor = false;
+            this.btnEjercicio2.Click += new System.EventHandler(this.btnEjercicio2_Click);
+            // 
+            // btnEjercicio3
+            // 
+            this.btnEjercicio3.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnEjercicio3.Location = new System.Drawing.Point(537, 39);
+            this.btnEjercicio3.Name = "btnEjercicio3";
+            this.btnEjercicio3.Size = new System.Drawing.Size(211, 64);
+            this.btnEjercicio3.TabIndex = 2;
+            this.btnEjercicio3.Text = "Ejercicio 3 -  Búsqueda de Texto";
+            this.btnEjercicio3.UseVisualStyleBackColor = false;
+            this.btnEjercicio3.Click += new System.EventHandler(this.btnEjercicio3_Click);
+            // 
+            // btnEjercicio4
+            // 
+            this.btnEjercicio4.BackColor = System.Drawing.Color.Magenta;
+            this.btnEjercicio4.Location = new System.Drawing.Point(30, 157);
+            this.btnEjercicio4.Name = "btnEjercicio4";
+            this.btnEjercicio4.Size = new System.Drawing.Size(211, 64);
+            this.btnEjercicio4.TabIndex = 3;
+            this.btnEjercicio4.Text = "Ejercicio 4 -  Estudiantes";
+            this.btnEjercicio4.UseVisualStyleBackColor = false;
+            this.btnEjercicio4.Click += new System.EventHandler(this.btnEjercicio4_Click);
+            // 
+            // btnEjercicio5
+            // 
+            this.btnEjercicio5.BackColor = System.Drawing.Color.Magenta;
+            this.btnEjercicio5.Location = new System.Drawing.Point(281, 157);
+            this.btnEjercicio5.Name = "btnEjercicio5";
+            this.btnEjercicio5.Size = new System.Drawing.Size(211, 64);
+            this.btnEjercicio5.TabIndex = 4;
+            this.btnEjercicio5.Text = "Ejercicio 5 -  Mínimo y Máximo";
+            this.btnEjercicio5.UseVisualStyleBackColor = false;
+            this.btnEjercicio5.Click += new System.EventHandler(this.btnEjercicio5_Click);
+            // 
+            // btnEjercicio6
+            // 
+            this.btnEjercicio6.BackColor = System.Drawing.Color.Turquoise;
+            this.btnEjercicio6.Location = new System.Drawing.Point(537, 157);
+            this.btnEjercicio6.Name = "btnEjercicio6";
+            this.btnEjercicio6.Size = new System.Drawing.Size(211, 64);
+            this.btnEjercicio6.TabIndex = 5;
+            this.btnEjercicio6.Text = "Ejercicio 6 - Búsqueda Exhaustiva";
+            this.btnEjercicio6.UseVisualStyleBackColor = false;
+            this.btnEjercicio6.Click += new System.EventHandler(this.btnEjercicio6_Click);
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 296);
+            this.Controls.Add(this.btnEjercicio6);
+            this.Controls.Add(this.btnEjercicio5);
+            this.Controls.Add(this.btnEjercicio4);
+            this.Controls.Add(this.btnEjercicio3);
+            this.Controls.Add(this.btnEjercicio2);
+            this.Controls.Add(this.btnEjercicio1);
+            this.Name = "Index";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjercicio1;
+        private System.Windows.Forms.Button btnEjercicio2;
+        private System.Windows.Forms.Button btnEjercicio3;
+        private System.Windows.Forms.Button btnEjercicio4;
+        private System.Windows.Forms.Button btnEjercicio5;
+        private System.Windows.Forms.Button btnEjercicio6;
+    }
+}
---
+++ Ejercicio1/Index.cs
@@ -0,0 +1,51 @@
+﻿using Ejercicio1.Ejercicios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Index : Form
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjercicio1_Click(object sender, EventArgs e)
+        {
+            new Ejercicio1().ShowDialog();
+        }
+
+        private void btnEjercicio2_Click(object sender, EventArgs e)
+        {
+            new Ejercicio2().ShowDialog();
+        }
+
+        private void btnEjercicio3_Click(object sender, EventArgs e)
+        {
+            new Ejercicio3().ShowDialog();
+        }
+
+        private void btnEjercicio4_Click(object sender, EventArgs e)
+        {
+            new Ejercicio4().ShowDialog();
+        }
+
+        private void btnEjercicio5_Click(object sender, EventArgs e)
+        {
+            new Ejercicio5().ShowDialog();
+        }
+
+        private void btnEjercicio6_Click(object sender, EventArgs e)
+        {
+            new Ejercicio6().ShowDialog();
+        }
+    }
+}
